e3027288dbf8f2470faba0284d34afef95458593
pgt-2020-27-teguh <anggitloufansa@gmail.com>
2020-03-16 14:16:49 +0700

    commit pertama

--- formpenjumlahan.php
+++ formpenjumlahan.php
@@ -13,6 +13,7 @@ if (mysqli_connect_errno()){
 </head>
 <body>
     <h1>cobagit</h1>
+    <p>cobagit</p>
 	<form method="post">	
 	nilai a <input type="text" name="a"><br>	
 	nilai b<input type="text" name="b"><br>
